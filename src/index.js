23c4c2f1cbbad19f20faa78eec4b13e72fe3231b
Angkit <angkithashamsa21@gmail.com>
2021-09-03 16:34:33 +0530

    'bug fixed'

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import App from './App'
 import * as serviceWorker from './serviceWorker'
 import { GithubProvider } from './context/context'
 import { Auth0Provider } from '@auth0/auth0-react'
-// dev-7sv6hk3f.us.auth0.com
-// mdg1l0G6yXhD36gPUdpowG1RxXw67EAp
 
 ReactDOM.render(
   <React.StrictMode>
